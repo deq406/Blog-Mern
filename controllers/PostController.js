3ce6ae539aab3e18e3a5a19b4b155544810ebaa3
deq406 <egormartin0@gmail.com>
2022-10-17 17:39:46 +0300

    fixed tags block

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -23,7 +23,10 @@ export const getLastTags = async (req, res) => {
       .map((obj) => obj.tags)
       .flat()
       .slice(0, 5);
-    res.json(tags);
+    const uniqueTags = tags.filter(function (item, pos) {
+      return tags.indexOf(item) == pos;
+    });
+    res.json(uniqueTags);
   } catch (err) {
     console.log(err);
     res.status(500).json({
